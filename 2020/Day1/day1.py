a5f5f7b7311a60a8c0c8becb382e63e192b5a0bf
Justin Presley <jpresley@broadinstitute.org>
2020-12-01 18:33:10 -0500

    2020 Day 1

---
+++ 2020/Day1/day1.py
@@ -0,0 +1,34 @@
+def partOne(inputDict):
+    for key, value in inputDict.items():
+        if value in inputDict.keys():
+            print(f"Part 1: Found {key}, {value}")
+            answer = key * value
+            print(f"Part 1: Answer is {answer}")
+            return
+
+def partTwo(inputDict):
+    for key1 in inputDict.keys():
+        for key2 in inputDict.keys():
+            for key3 in inputDict.keys():
+                if key1 + key2 + key3 == 2020:
+                    print(f"Part 2: Found {key1}, {key2}, {key3}")
+                    answer = key1 * key2 * key3
+                    print(f"Part 2: Answer is {answer}")
+                    return
+
+def main():
+
+    inputDict = {}
+
+    with open("Input.txt") as inputFile:
+        for line in inputFile:
+            compliment = 2020 - int(line)
+            inputDict[int(line.rstrip())] = compliment
+    
+    partOne(inputDict)
+
+    partTwo(inputDict)
+
+
+if __name__ == "__main__":
+    main()
