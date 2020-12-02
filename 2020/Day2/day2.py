59d1a4952d860af7b87d242532652fda5b257039
Justin Presley <jpresley@broadinstitute.org>
2020-12-02 09:31:02 -0500

    Day 2 part 2

--- 2020/Day2/day2.py
+++ 2020/Day2/day2.py
@@ -3,6 +3,7 @@ import re
 inputParse = re.compile('''^(\d+)-(\d+) (\S): (.+)''')
 
 goodPassCount = 0
+goodPassCount2 = 0
 
 with open('Input.txt') as inputFile:
     for line in inputFile:
@@ -10,10 +11,22 @@ with open('Input.txt') as inputFile:
         # print(parsedLine.group(4))
         # Groups: 1-lowest, 2-highest, 3-character, 4-password
         charCounter = 0
+        charCounter2 = 0
+        # Part 1
         for character in parsedLine.group(4):
             if character is parsedLine.group(3):
                 charCounter += 1
         if charCounter >= int(parsedLine.group(1)) and charCounter <= int(parsedLine.group(2)):
             goodPassCount += 1
+        # Part 2
+        position1 = int(parsedLine.group(1)) - 1
+        position2 = int(parsedLine.group(2)) - 1
+        if parsedLine.group(4)[position1] is parsedLine.group(3):
+            charCounter2 += 1
+        if parsedLine.group(4)[position2] is parsedLine.group(3):
+            charCounter2 += 1
+        if charCounter2 is 1:
+            goodPassCount2 += 1
 
 print(goodPassCount)
+print(goodPassCount2)
