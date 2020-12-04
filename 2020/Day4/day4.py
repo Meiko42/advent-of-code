c7c0a43ad22006314c2b5a00252869559c4861c9
Justin Presley <jpresley@broadinstitute.org>
2020-12-04 13:34:16 -0500

    Day 4 part 2 but it makes me sad

--- 2020/Day4/day4.py
+++ 2020/Day4/day4.py
@@ -1,5 +1,6 @@
+# This code makes me sad. I'm going to redo when I have a good chunk of time to focus
+
 import re
-import pprint
 
 # byr (Birth Year)
 # iyr (Issue Year)
@@ -10,9 +11,22 @@ import pprint
 # pid (Passport ID)
 # cid (Country ID) - Missing this is fine for Part 1
 
+def ValueChecker(*args, **kwargs):
+    pass
+
 day1Requirements = {'byr': True, 'iyr': True, 'eyr': True, 'hgt': True, 'hcl': True, 'ecl': True, 'pid': True, 'cid': False}
 
+# For Part 1 we accept fields that are present without validating their values
 lolMatch = re.compile('(\S+):(\S+)')
+
+# For part 2 we start to validate the fields 
+hgtValidMatch = re.compile('((15[0-9]|16[0-9]|17[0-9]|18[0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in)')
+hclValidMatch = re.compile('^#[a-f0-9]{6}')
+eclValidMatch = re.compile('^(amb|blu|brn|gry|grn|hzl|oth)$')
+pidValidMatch = re.compile('^([0-9]{9})$')
+
+
+
 
 passportsDict = {}
 passportsCount = 1
@@ -29,7 +43,8 @@ with open('Input.txt') as inputFile:
             passportsCount += 1
             passportsDict[passportsCount] = {}
 
-numberOfInvalidPassports = 0
+numberOfInvalidPassportsP1 = 0
+numberOfInvalidPassportsP2 = 0
 
 for passport, passportEntries in passportsDict.items():
     for requirement in day1Requirements.keys():
@@ -38,8 +53,40 @@ for passport, passportEntries in passportsDict.items():
         
 for passport, passportEntries in passportsDict.items():
     if ('validityPart1' in passportEntries.keys()) and (passportEntries['validityPart1'] == False):
-        numberOfInvalidPassports += 1
+        numberOfInvalidPassportsP1 += 1
+        passportEntries['validityPart2'] = False
+    else:
+        # At this point we know the entries will have all required fields
+        # byr
+        if (int(passportEntries['byr']) < 1920) or (int(passportEntries['byr']) > 2002):
+            passportEntries['validityPart2'] = False
+        # iyr
+        if (int(passportEntries['iyr']) < 2010) or (int(passportEntries['iyr']) > 2020):
+            passportEntries['validityPart2'] = False
+        # eyr
+        if (int(passportEntries['eyr']) < 2020) or (int(passportEntries['eyr']) > 2030):
+            passportEntries['validityPart2'] = False
+        # hgt
+        if not hgtValidMatch.match(passportEntries['hgt']):
+            passportEntries['validityPart2'] = False
+        # hcl
+        if not hclValidMatch.match(passportEntries['hcl']):
+            passportEntries['validityPart2'] = False
+        # ecl
+        if not eclValidMatch.match(passportEntries['ecl']):
+            passportEntries['validityPart2'] = False
+        # pid
+        if not pidValidMatch.match(passportEntries['pid']):
+            passportEntries['validityPart2'] = False
+            print("wow")
+
+for passport, passportEntries in passportsDict.items():
+    if ('validityPart2' in passportEntries.keys()) and (passportEntries['validityPart2'] == False):
+        numberOfInvalidPassportsP2 += 1
 
 totalPassports = len(passportsDict)
 
-print(totalPassports - numberOfInvalidPassports)
+# Part 1 - Answer is 206
+print(totalPassports - numberOfInvalidPassportsP1)
+# Part 2 - Answer is 123
+print(totalPassports - numberOfInvalidPassportsP2)
