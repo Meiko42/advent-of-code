78334a43fdfcf34c5db20abbc838c0c7d4066a1d
Justin Presley <jpresley@broadinstitute.org>
2020-12-11 13:40:59 -0500

    Day 9 Part 2

--- 2020/Day9/day9.py
+++ 2020/Day9/day9.py
@@ -3,8 +3,6 @@ with open("Input.txt") as inputFile:
     for line in inputFile:
         inputList.append(int(line.strip()))
 
-
-
 def firstInvalid(inputList, preamble):
     preamble = preamble - 1
     startingIndex = 0
@@ -17,9 +15,6 @@ def firstInvalid(inputList, preamble):
     while value <= len(inputList):
         while pointer1 != endingIndex:
             while pointer2 <= endingIndex:
-                # print("CYCLE")
-                # print(inputList[pointer1])
-                # print(inputList[pointer2])
                 if inputList[pointer1] + inputList[pointer2] == inputList[value]:
                     valid = True
                 pointer2 += 1
@@ -35,4 +30,31 @@ def firstInvalid(inputList, preamble):
         value = endingIndex + 1
         valid = False
 
-print(firstInvalid(inputList, 25))
+def scanSum(inputList, target):
+    startingIndex = 0
+    step = 0 
+    total = 0
+    # Low, High
+    endsList = [2147483647, 0]
+    while True:
+        while total < target:
+            value = inputList[step]
+            total += value
+            step += 1
+            if value < endsList[0]:
+                endsList[0] = value
+            if value > endsList[1]:
+                endsList[1] = value
+            if total == target:
+                return endsList[0] + endsList[1]
+        startingIndex += 1
+        step = startingIndex
+        total = 0
+        endsList = [2147483647, 0]
+
+# Part 1 - Answer 69316178
+part1Answer = firstInvalid(inputList, 25)
+print(part1Answer)
+
+# Part 2 - Answer 9351526
+print(scanSum(inputList, part1Answer))
