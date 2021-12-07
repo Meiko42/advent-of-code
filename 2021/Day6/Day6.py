bb3354c8029ff812dec64f890f1e3bd94e1b8d5d
jpresley@broadinstitute.org <justinpresley@MBP14inch2021.thats-no-moon.com>
2021-12-07 02:49:10 -0500

    Day 6 part 2

--- 2021/Day6/Day6.py
+++ 2021/Day6/Day6.py
@@ -38,28 +38,51 @@ def pass_one_day(allFish):
 
 
 def main():
-    # input_file = "input.txt"
-    input_file = "input-test.txt"
+    input_file = "input.txt"
+    # input_file = "input-test.txt"
     inputs = read_input(input_file)
 
     inputs = inputs[0].split(',')
 
-    allFish = []
+    allFish = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+    }
 
     for fish in inputs:
-        allFish.append(Angler(age=int(fish)))
+        allFish[int(fish)] += 1
 
     # for i in allFish:
     #     print(i.cycleTimer)
 
-
-    experimentLength = 80
-
+    experimentLength = 256
     for i in range(experimentLength):
-        allFish = pass_one_day(allFish)
+        resetExisting = allFish[0]
+        newFishCounter = allFish[0]
+        allFish[0] = allFish[1]
+        allFish[1] = allFish[2]
+        allFish[2] = allFish[3]
+        allFish[3] = allFish[4]
+        allFish[4] = allFish[5]
+        allFish[5] = allFish[6]
+        allFish[6] = allFish[7]
+        allFish[6] += resetExisting
+        allFish[7] = allFish[8]
+        allFish[8] = newFishCounter
+        print(allFish)
 
+    totalFish = 0
+    for key, value in allFish.items():
+        totalFish += value
 
-    print(len(allFish))
+    print(totalFish)
 
 if __name__ == "__main__":
     main()
