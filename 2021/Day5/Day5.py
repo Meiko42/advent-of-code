8072e4792a6e5bfced15705aa6ed1dedc6b72b94
jpresley@broadinstitute.org <justinpresley@MBP14inch2021.thats-no-moon.com>
2021-12-06 23:57:00 -0500

    Day 5 part1 working

--- 2021/Day5/Day5.py
+++ 2021/Day5/Day5.py
@@ -1,37 +1,72 @@
+import pprint
+
 def read_input(input_file):
     inputs = [x.strip().split(' -> ') for x in open(input_file)]
 
     return inputs
 
 
-def check_line_points(lineStart, lineEnd, linePoints, overlappingLinePoints):
+# def check_line_points(lineStart, lineEnd, linePoints, overlappingLinePoints):
 
-    print(lineStart)
-    print(lineEnd)
-    
-    position = lineStart
-    for i in range(lineStart, lineEnd + 1):
-        # print(position)
-        if position not in linePoints:
-            linePoints.add(position)
-        elif position in linePoints:
-            overlappingLinePoints.add(position)
-        else:
-            raise Exception("""We shouldn't be able to reach this point""")
-        position += 1
+#     if lineStart < lineEnd:
+#         temp = lineStart
+#         lineStart = lineEnd
+#         lineEnd = temp
 
-    return linePoints, overlappingLinePoints
+#     linePosition = lineStart
+#     for i in range(lineStart, lineEnd + 1):
+#         print(linePosition)
+#         if linePosition not in linePoints:
+#             linePoints.add(linePosition)
+#         elif linePosition in linePoints:
+#             overlappingLinePoints.add(linePosition)
+#         else:
+#             raise Exception("""We shouldn't be able to reach this point""")
 
+#         linePosition += 1
+#     print(linePoints)
+#     print(overlappingLinePoints)
+#     return linePoints, overlappingLinePoints
+
+def check_line_points(lineStart, lineEnd, linePoints, overlapCounter, stableX=None, stableY=None):
+
+    if lineStart > lineEnd:
+        temp = lineStart
+        lineStart = lineEnd
+        lineEnd = temp
+
+    if stableX:
+        if stableX not in linePoints:
+            linePoints[stableX] = {}
+        for i in range(lineStart, lineEnd+1):
+            if i not in linePoints[stableX]:
+                linePoints[stableX][i] = False
+            else:
+                linePoints[stableX][i] = True
+                overlapCounter += 1
+    elif stableY:
+        for i in range(lineStart, lineEnd+1):
+            if i not in linePoints:
+                linePoints[i] = {}
+            if stableY not in linePoints[i]:
+                linePoints[i][stableY] = False
+            else:
+                linePoints[i][stableY] = True
+                overlapCounter += 1
+    else:
+        raise Exception('Incorrect values supplied to check_line_points.')
+
+    return linePoints, overlapCounter
 
 def main():
-    input_file = "input-test.txt"
-    # input_file = "input.txt"
+    # input_file = "input-test.txt"
+    input_file = "input.txt"
     inputs = read_input(input_file)
 
     # print(inputs)
 
-    linePoints = set()
-    overlappingLinePoints = set()
+    linePoints = {}
+    overlapCounter = 0
 
     for line in inputs:
         linePointA = line[0].split()
@@ -45,11 +80,25 @@ def main():
         linePointBX = int(linePointZ[0])
         linePointBY = int(linePointZ[1])
         if linePointAX == linePointBX:
-            linePoints, overlappingLinePoints = check_line_points(linePointAY, linePointBY, linePoints, overlappingLinePoints)
-        if linePointAY == linePointBY:
-            linePoints, overlappingLinePoints = check_line_points(linePointAX, linePointBX, linePoints, overlappingLinePoints)
+            linePoints, overlapCounter = check_line_points(linePointAY, linePointBY, linePoints, overlapCounter, stableX=linePointAX)
+        elif linePointAY == linePointBY:
+            linePoints, overlapCounter = check_line_points(linePointAX, linePointBX, linePoints, overlapCounter, stableY=linePointAY)
+        else:
+            # print(f"Skipping diaganal line: {line}")
+            pass
 
-    print(len(overlappingLinePoints))
+    # pprint.pprint(linePoints)
+
+    # print(overlapCounter)
+
+    doubleCheckCounter = 0
+
+    for x, values in linePoints.items():
+        for y, hitcount in values.items():
+            if hitcount:
+                doubleCheckCounter += 1
+    
+    print(doubleCheckCounter)
 
 if __name__ == "__main__":
     main()
