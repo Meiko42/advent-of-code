fd8e258716b077aeec21247006983a729d23be55
Justin Presley <justin.r.presley@gmail.com>
2024-12-15 10:01:26 -0500

    AoC 2024 Day 1 Part 2

--- 2024/Python/Day1/Day1.py
+++ 2024/Python/Day1/Day1.py
@@ -3,11 +3,27 @@
 leftList = []
 rightList = []
 
+# {leftNum: {left_occurances: 1, right_occurances: 0}}
+similarityDict = {}
+
 with open("input", "r") as inputFile:
     for line in inputFile:
         left, right = map(int, line.split())
+
         leftList.append(left)
+
+        if left in similarityDict.keys():
+            similarityDict[left]["left_occurances"] += 1
+        else:
+            similarityDict[left] = {"left_occurances": 1, "right_occurances": 0}
+
         rightList.append(right)
+
+        if right in similarityDict.keys():
+            similarityDict[right]["right_occurances"] += 1
+        else:
+            similarityDict[right] = {"left_occurances": 0, "right_occurances": 1}
+
 
 leftList.sort()
 rightList.sort()
@@ -19,3 +35,10 @@ for position, leftValue in enumerate(leftList):
     part1 += distance
 
 print(f"Part one answer: {part1}")
+
+similarityScore = 0
+
+for locationID, locationIDComparison in similarityDict.items():
+    similarityScore += locationID * locationIDComparison["right_occurances"] * locationIDComparison["left_occurances"]
+
+print(f"Part two answer: {similarityScore}")
